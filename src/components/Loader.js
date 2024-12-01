da6ff1c24f88c7be94c8860a6fef8c3fb5a8a1f4
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-12-01 19:18:55 +0500

    new

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const Loader = () => {
     return (
-        <div style={{ color: "white", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+        <div
+            style={{
+                color: "white",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+            }}
+        >
             Loading...
         </div>
     );
--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Text } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
 const PortfolioCard = ({ position, title }) => {
     return (
         <mesh position={position}>
             <boxGeometry args={[2, 3, 0.1]} />
             <meshStandardMaterial color="white" />
-            <Text position={[0, 1.5, 0]} fontSize={0.5} color="black">
-                {title}
-            </Text>
+            <Html position={[0, 1.5, 0]} center>
+                <div style={{ textAlign: "center", color: "black" }}>{title}</div>
+            </Html>
         </mesh>
     );
 };
