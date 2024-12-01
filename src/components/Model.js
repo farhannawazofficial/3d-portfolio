84990eedd1877c29ef2480a63eb16e462cac2355
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-12-01 19:21:30 +0500

    Update Model.js

--- src/components/Model.js
+++ src/components/Model.js
@@ -3,7 +3,7 @@ import { useGLTF } from "@react-three/drei";
 
 const Model = ({ path }) => {
     const { scene } = useGLTF(path);
-    return <primitive object={scene} scale={1.5} />;
+    return <primitive object={scene} scale={[1.5, 1.5, 1.5]} />;
 };
 
 export default Model;
